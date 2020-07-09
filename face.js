32575f468c05c2f5c2c8d30c49840fa885976c56
sakamoto570 <67948622+sakamoto570@users.noreply.github.com>
2020-07-09 15:31:36 +0900

    Add files via upload

--- face.js
+++ face.js
@@ -8,7 +8,7 @@ $(function() {
 
         var subscriptionKey = "2859b5e18586486f948c84afadeedcad";
 
-        var uriBase = "https://faceaaaaa.cognitiveservices.azure.com/";
+        var uriBase = "https://faceaaaaa.cognitiveservices.azure.com/detect";
         var params = {
             "returnFaceId": "true",
             "returnFaceLandmarks": "false",
